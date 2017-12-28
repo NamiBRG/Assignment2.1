02e092c565ba244f9528aa221f0540cc25c12c37
NamiBRG <nami.brg@gmail.com>
2017-12-28 22:49:29 +0530

    Add files via upload

---
+++ acad.java
@@ -0,0 +1,14 @@
+
+public class acad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,z;
+		x=10;
+		y=30;
+		z=x+y;
+				System.out.println("The sum is: "+z);
+
+	}
+
+}
