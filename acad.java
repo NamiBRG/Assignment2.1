4fa47acb888181956e1a6858d7cfb1610615adf2
NamiBRG <nami.brg@gmail.com>
2017-12-28 23:08:21 +0530

    Add files via upload
    Sum of two integers given by users at runtime.

--- acad.java
+++ acad.java
@@ -1,11 +1,15 @@
+import java.util.*;
 
 public class acad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x,y,z;
-		x=10;
-		y=30;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first integer: ");
+		x=sc.nextInt();
+		System.out.println("Enter the second integer: ");
+		y=sc.nextInt();
 		z=x+y;
 				System.out.println("The sum is: "+z);
 
