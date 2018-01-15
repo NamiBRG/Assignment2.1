fb6b8fcb2e4c93ba3ff6a0c41bee729f140c568f
NamiBRG <nami.brg@gmail.com>
2018-01-15 12:04:47 +0530

    Add files via upload

---
+++ HardCode.java
@@ -0,0 +1,17 @@
+
+class HardCode 
+{
+    
+  public static void main(String[] arg )
+  {
+	  int x;
+	  int a;
+	  
+	  x=20;
+	  a=20;
+    int sum;
+    sum = x+a;
+    System.out.println("Sum of the two numbers is: " +sum);
+
+  }
+}
